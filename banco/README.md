07e5154fb2836545920f0b09f4568a09821bd979
Lucas Sachetto <lucashsachetto@hotmail.com>
2023-05-08 17:20:47 -0300

    readme

--- banco/README.md
+++ banco/README.md
@@ -1,18 +1,7 @@
-## Getting Started
+## COMO RODAR
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+Adicione todos o .jar da pasta lib no seu projeto e rode.
 
-## Folder Structure
+Caso apareça a mensagem: "Erro ao se conectar com o BD:" pergunte ao grupo qual é o IP atual do banco de dados e substitua no arquivo ./banco/src/main/Banco.java linha 14.
 
-The workspace contains two folders by default, where:
-
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
-
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
-
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
-
-## Dependency Management
-
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+Rode o Launch App!!
--- banco/src/main/App.java
+++ banco/src/main/App.java
@@ -2,20 +2,21 @@ package main;
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Banco.connectDb();
+        // APENAS UTILIZADO PARA TESTE
+        // Banco.connectDb();
 
-        Usuario u1 = UsuarioHelper.getUsuario("Lucas", "123");
-        Usuario u2 = UsuarioHelper.getUsuario("teste","456");
-        ContaCorrente cc2 = new ContaCorrente(ContaHelper.getContaByUserIdTipo(u2.getId(), "cc"));
-        ContaCorrente cc = new ContaCorrente(ContaHelper.getContaByUserIdTipo(u1.getId(), "cc"));
+        // Usuario u1 = UsuarioHelper.getUsuario("Lucas", "123");
+        // Usuario u2 = UsuarioHelper.getUsuario("teste","456");
+        // ContaCorrente cc2 = new ContaCorrente(ContaHelper.getContaByUserIdTipo(u2.getId(), "cc"));
+        // ContaCorrente cc = new ContaCorrente(ContaHelper.getContaByUserIdTipo(u1.getId(), "cc"));
 
-        ContaInvestimento ci = new ContaInvestimento(ContaHelper.getContaByUserIdTipo(u1.getId(), "ci"));
+        // ContaInvestimento ci = new ContaInvestimento(ContaHelper.getContaByUserIdTipo(u1.getId(), "ci"));
         
-        //cc.pix("555469", 200.00);
+        // //cc.pix("555469", 200.00);
         
-        cc.verHistorico();
-        ci.verHistorico();
-        cc2.verHistorico();
-        Banco.disconnectDb();
+        // cc.verHistorico();
+        // ci.verHistorico();
+        // cc2.verHistorico();
+        // Banco.disconnectDb();
     }
 }
